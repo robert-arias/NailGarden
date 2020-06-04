5132eae6de2bc486783852f10d3e4215697ad19b
robert-arias <robertariascastro@outlook.com>
2020-06-03 19:45:42 -0600

    menu items

--- javascript/main.js
+++ javascript/main.js
@@ -1,18 +1,6 @@
-//Click over the burger menu.
+//Burger button opens
+$('.nav-burger').click(function(){
+      $(this).toggleClass("open");
+      $(".nav-items").toggleClass("open");
+   });
 
-//Selects the .nav-burger class.
-const burgerBtn = document.querySelector('.nav-burger');
-//Variable used to know if the burger button was clicked on. False if action is to open the menu.
-let burgerOpen = false;
-//Click event listener for burger button click action.
-burgerBtn.addEventListener('click', () => {
-    if(!burgerOpen) {
-        //Adds class 'open' to animate click action.
-        burgerBtn.classList.add('open');
-        burgerOpen = true;
-  } else {
-        //Removes class 'open' to animate click action.
-        burgerBtn.classList.remove('open');
-        burgerOpen = false;
-  }
-});
