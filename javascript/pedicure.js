08ff70f1252b9d54735cb8a8dfa7e34d91e5a4b5
robert-arias <robertariascastro@outlook.com>
2020-06-28 17:05:04 -0600

    carrausel done

--- javascript/pedicure.js
+++ javascript/pedicure.js
@@ -1,24 +1,31 @@
-var count = $('.type').children().length;
+//Burger button opens
+$('.nav-burger').click(function(){
+      $(this).toggleClass("open");
+      $(".nav-items").toggleClass("open");
+      $(".logo").toggleClass("open");
+});
+
+var count = $('.pedicure-type-container').children().length;
 const size = 180;
 let counter = 1;
 $('.arrowLeft').click(function(){
       if(counter > 1) {
             --counter;
-            $('.review').css('transform', 'translateX(' + -size*(counter-1) + '%)');
+            $('.type').css('transform', 'translateX(' + -size*(counter-1) + '%)');
       }
       else {
             counter = count;
-            $('.review').css('transform', 'translateX(' + -size*(counter-1) + '%)');
+            $('.type').css('transform', 'translateX(' + -size*(counter-1) + '%)');
       }
 });
 
 $('.arrowRight').click(function(){
       if(counter < count) {
-            $('.type-description').css('transform', 'translateX(' + -size*counter + '%)');
+            $('.type').css('transform', 'translateX(' + -size*counter + '%)');
             counter++;
       }
       else {
-            $('.type-description').css('transform', 'translateX(' + 0 + '%)');
+            $('.type').css('transform', 'translateX(' + 0 + '%)');
             counter = 1;
       }
 });
