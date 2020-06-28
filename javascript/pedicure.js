5874ca8958b95d176848d77a7ebdeeb1b888ac88
norma430 <40445376+norma430@users.noreply.github.com>
2020-06-28 15:25:23 -0600

    Desktop

--- javascript/pedicure.js
+++ javascript/pedicure.js
@@ -1,1 +1,24 @@
+var count = $('.type').children().length;
+const size = 180;
+let counter = 1;
+$('.arrowLeft').click(function(){
+      if(counter > 1) {
+            --counter;
+            $('.review').css('transform', 'translateX(' + -size*(counter-1) + '%)');
+      }
+      else {
+            counter = count;
+            $('.review').css('transform', 'translateX(' + -size*(counter-1) + '%)');
+      }
+});
 
+$('.arrowRight').click(function(){
+      if(counter < count) {
+            $('.type-description').css('transform', 'translateX(' + -size*counter + '%)');
+            counter++;
+      }
+      else {
+            $('.type-description').css('transform', 'translateX(' + 0 + '%)');
+            counter = 1;
+      }
+});
